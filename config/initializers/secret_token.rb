849bed72f3ed96340660705ad25efc1a4ac5695a
sivagollapalli <siva@joshsoftware.com>
2014-02-22 23:56:13 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RentAnItem::Application.config.secret_key_base = '29a2b31c01d58b22531bc6d1e7a72cd78857dae6672b9246e7b581d05be18215123fb20380329ee10fb34b3fb4bb8c8ae6ddd4a20ba1cb17a107df00f0dadbf5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RentAnItem::Application.config.session_store :cookie_store, key: '_rent_an_item_session'
